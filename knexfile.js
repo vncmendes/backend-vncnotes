d71ab6ae6e44552b02293b1171a9ffa32974a081
- Vini Mendes <vncmendes>
2024-09-05 09:48:01 -0300

    deploy with docker

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,10 @@
 const path = require("path");
 
-//configs
+// configs
 module.exports = {
   development: {
-    client: 'postgresql',
-    connection: {
-      postgres_URL: 'postgres://default:KfX0md1yEvWa@ep-autumn-fire-a4xbe7yb-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require',
-      postgres_HOST: 'ep-autumn-fire-a4xbe7yb-pooler.us-east-1.aws.neon.tech',
-      postgres_DATABASE: 'postgres',
-      postgres_USER: 'default',
-      postgres_PASSWORD: 'KfX0md1yEvWa'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
@@ -18,18 +12,14 @@ module.exports = {
     migrations: {
       directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
     },
-
-    useNullAsDefault: true
   },
 
   production: {
     client: 'postgresql',
     connection: {
-      postgres_URL: 'postgres://default:KfX0md1yEvWa@ep-autumn-fire-a4xbe7yb-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require',
-      postgres_HOST: 'ep-autumn-fire-a4xbe7yb-pooler.us-east-1.aws.neon.tech',
-      postgres_DATABASE: 'postgres',
-      postgres_USER: 'default',
-      postgres_PASSWORD: 'KfX0md1yEvWa'
+      host: 'localhost',
+      username: 'postgres',
+      password: "prodpw"
     },
     pool: {
       min: 2,
